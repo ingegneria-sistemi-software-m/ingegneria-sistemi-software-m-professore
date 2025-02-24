16a533e7a68a7aa3b6725975f583a5bb752b9c2e
Antonio Natali <agoognat@gmail.com>
2025-02-24 19:06:10 +0100

    Feb25

---
+++ conwaygui0/src/main/java/iss25/conwaygui0/Conwaygui0Application.java
@@ -0,0 +1,13 @@
+package iss25.conwaygui0;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Conwaygui0Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Conwaygui0Application.class, args);
+	}
+
+}
---
+++ conwaygui0/src/test/java/iss25/conwaygui0/Conwaygui0ApplicationTests.java
@@ -0,0 +1,13 @@
+package iss25.conwaygui0;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Conwaygui0ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
