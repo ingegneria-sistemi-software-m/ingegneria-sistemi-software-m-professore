ef1dc8a068dd607cf3fbb5877e69befec3aa4031
Antonio Natali <antonio.natali@unibo.it>
2025-04-03 12:13:33 +0200

    April3

--- conway25qak0/conway25qak0.pl
+++ conway25qak0/conway25qak0.pl
@@ -5,6 +5,7 @@ dispatch( startGame, startGame(ARG) ).
 dispatch( stopGame, stopGame(ARG) ).
 dispatch( clearGame, clearGame(ARG) ).
 dispatch( exitGame, exitGame(ARG) ).
+dispatch( cellstate, cellstate(ARG) ).
 %====================================================================================
 context(ctxconway0, "localhost",  "TCP", "8920").
  qactor( conway0, ctxconway0, "it.unibo.conway0.Conway0").
