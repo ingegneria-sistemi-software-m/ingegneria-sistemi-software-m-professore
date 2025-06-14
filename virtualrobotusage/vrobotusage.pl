1fd6ade2a6b701cc8fd1b6f601f649a56d3c07b1
Antonio Natali <agoognat@gmail.com>
2025-06-14 18:27:49 +0200

    June14 readme

--- virtualrobotusage/vrobotusage.pl
+++
@@ -1,8 +0,0 @@
-%====================================================================================
-% vrobotusage description   
-%====================================================================================
-dispatch( cmd, cmd(MOVE) ). %MOVE = w|s|a|d   mosse del virtual robot
-%====================================================================================
-context(ctxvrobotusage, "localhost",  "TCP", "8120").
- qactor( vrclient, ctxvrobotusage, "it.unibo.vrclient.Vrclient").
- static(vrclient).
